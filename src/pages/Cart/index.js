8108eaa3f4009d2433f9db0c13e1439b10e7a043
Danilo <h2danilo@hotmail.com>
2021-01-19 16:32:27 -0300

    Removendo Produto Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -8,7 +8,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -46,7 +46,12 @@ function Cart({ cart }) {
                 <strong>{product.amount * product.price}</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} color="#00a03e" />
                 </button>
               </td>
